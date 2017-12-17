d135dfddbb9a778c655822f7e34f2c6ffef53d7b
Youssef Sherif <sharief@aucegypt.edu>
2017-12-18 01:25:38 +0200

    Clean up

--- src/core/event-handlers.js
+++ src/core/event-handlers.js
@@ -40,7 +40,7 @@ async function handleAccountReinstated(e) {
     const collection = await db.db('accounts-event-store').collection('events')
     collection.find({ 'aggregateId': e.aggregateId }).toArray(function(err, events) {
         events = events.filter(x => x.name !== eventsConstants.domainEvents.accountApproved && x.name !== eventsConstants.domainEvents.accountDeleted && x.name !== eventsConstants.domainEvents.accountReinstated)
-        const q = queue()
+        const q = handleAccountReinstatedQueue()
         events.forEach(async e => {
             return await q(e)
         })
@@ -49,19 +49,15 @@ async function handleAccountReinstated(e) {
     
 }
 
-function queue () {
+function handleAccountReinstatedQueue () {
     const arr = []
     return async function enqueue (e) {
         arr.push(e)
-        global.log.error(arr.length)
-        
         if(arr.length===1) return await recurs()
     }
     async function recurs() {
         await checkForUndoDeleteEvents(arr[0])
         arr.shift()
-        global.log.warn(arr.length)
-        
         if (arr.length) return recurs()
     }
     async function checkForUndoDeleteEvents(e){
